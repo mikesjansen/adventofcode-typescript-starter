932d14f12bee1cac28a97ac368d4584a0467db58
Mike Jansen <michael.jansen@gmail.com>
2022-12-03 10:12:19 -0800

    kill for loop

--- src/day3/index.ts
+++ src/day3/index.ts
@@ -9,11 +9,9 @@ class Day3 extends Day {
     solveForPartOne(input: string): string {
         const results = input.split(/\r?\n/)
             .map((m) => {
-                
                 const first = new Set(m.substring(0, m.length/2).split(''));
                 const second = new Set(m.substring(m.length/2).split(''));
                 const intersection = [...first.values()].find(f => second.has(f));
-
                 if (intersection! >= 'a' && intersection! <= 'z') return intersection!.charCodeAt(0) - 'a'.charCodeAt(0) + 1
                 return intersection!.charCodeAt(0) - 'A'.charCodeAt(0) + 1 + 26;
             });
@@ -21,17 +19,20 @@ class Day3 extends Day {
     }
 
     solveForPartTwo(input: string): string {
-        let sum = 0;
-        const lines = input.split(/\r?\n/);
-        for(let i=0; i < lines.length; i+=3) {
-            const first = new Set(lines[i].split(''))
-            const second = new Set(lines[i+1].split(''))
-            const third = new Set(lines[i+2].split(''))
-            const intersection = [...first.values()].find(f => second.has(f) && third.has(f));
-            if (intersection! >= 'a' && intersection! <= 'z') sum += intersection!.charCodeAt(0) - 'a'.charCodeAt(0) + 1
-            else sum += intersection!.charCodeAt(0) - 'A'.charCodeAt(0) + 1 + 26;
-        }
-        return sum.toString();
+        return input.split(/\r?\n/)
+            .reduce((prev, curr, i) => {
+                prev[Math.floor(i/3)] = (prev[Math.floor(i/3)] ?? []).concat(curr);
+                return prev;
+            }, [] as string[][])
+            .reduce((prev, curr) => {
+                const first = new Set(curr[0].split(''))
+                const second = new Set(curr[1].split(''))
+                const third = new Set(curr[2].split(''))
+                const intersection = [...first.values()].find(f => second.has(f) && third.has(f));
+                if (intersection! >= 'a' && intersection! <= 'z') return prev + intersection!.charCodeAt(0) - 'a'.charCodeAt(0) + 1
+                return prev + intersection!.charCodeAt(0) - 'A'.charCodeAt(0) + 1 + 26;
+            }, 0)
+            .toString();
     }
 }
 
